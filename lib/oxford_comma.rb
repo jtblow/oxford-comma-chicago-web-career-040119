d0ee5bc4d21918f7497777e58d2ed6eda7a240bf
creative-zombie-4458 <jtbartlow@gmail.com>
2019-03-21 05:03:31 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,6 @@ if array.length === 1
 elsif array.length === 2 
 array.join(" and ")
 elsif array.length === 3
-array.slice(0..1) + ",and " + array.slice(2)
+array.slice(0..1) + array.slice(2)
 end
 end
