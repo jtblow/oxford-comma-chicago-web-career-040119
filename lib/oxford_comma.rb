1660022dad6915253d56f23a7c3c8622a01fff07
creative-zombie-4458 <jtbartlow@gmail.com>
2019-03-21 05:21:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ if array.length === 1
 elsif array.length === 2 
 array.join(" and ")
 elsif array.length === 3
-array.insert(2, " and ")
-array.join(", ")
+insert(2, " and ")
+array[0].join(", ")
 end
 end
